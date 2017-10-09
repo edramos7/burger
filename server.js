c15cf602217100fddfa20e817e7d26fad9a20f3d
edramos7 <eord7@hotmail.com>
2017-10-08 23:02:36 -0500

    homework

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayaout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require(".controllers/burgersController.js");
+
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
+
+app.listen(PORT, function() {
+	console.log("Listening on port:%s", PORT);
+})
+
+
